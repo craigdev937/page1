509e8458d247e01ecc2091ae1717faec17c063f0
Craig <indigomx9@gmail.com>
2023-01-29 20:32:03 -0500

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Page1
+
+Initial setup of the project.  Created with Webpack, React, TypeScript, and Framer Motion.
+
+
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta reprehenderit aspernatur, distinctio id consequatur ipsa? Debitis recusandae veritatis quos repudiandae ipsa laborum quam quaerat sit, numquam modi eius voluptate amet.</p>
+        </React.Fragment>
+    );
+};
+
+
